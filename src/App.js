1035180a081a6c4b7e226d5d376951feae7993bd
unknown <khoi@gmail.com>
2020-09-07 12:27:59 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './components/Signup page/Signup';
 import Navbar from './components/Navbar';
-import About from './components/Info page/About';
 import './App.css';
 
 const App = () => (
