243ba329d65cc28af31fb1ac4acaa526457c946e
unknown <khoi@gmail.com>
2020-09-11 10:51:17 +0700

    updating challenge page

--- src/components/pages/Challengepage/Challenge.js
+++ src/components/pages/Challengepage/Challenge.js
@@ -1,59 +1,89 @@
 import React, { useState, useEffect } from 'react';
 import './Challenge.css';
 import { FaFire, FaHandshake } from 'react-icons/fa';
-import { GiBookCover } from 'react-icons/gi';
-import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
-import { IconContext } from 'react-icons/lib';
-import { Progress } from 'antd';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { RiArrowDropDownFill, RiArrowDropUpFill } from 'react-icons/ri';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 function Challenge() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
   return (
-    <IconContext.Provider value={{ color: '#fff', size: 64 }}>
-      <div className="challenge__section">
-        <div className="challenge__wrapper">
-          <h1 className="challenge__heading">Thử thách</h1>
-          <div className="challenge__container">
-            <div className={click ? 'challenge__container-card active' : 'challenge__container-card'}>
-              <div className={click ? 'challenge__container-cardInfo active' : 'challenge__container-cardInfo'}>
-                <div className="icon">
-                  <FaFire />
-                </div>
+
+    <div className="challenge__section">
+      <div className="challenge__wrapper">
+        <h1 className="challenge__heading">Giai đoạn tuyển chọn</h1>
+        <div className="challenge__container">
+          <div className={click ? 'challenge__container-card active' : 'challenge__container-card'}>
+            <div className={click ? 'challenge__container-cardInfo active' : 'challenge__container-cardInfo'}>
+              <div className="challenge-card-header">
+                <FaFire className="challenge-icon" />
                 <h3>Giai đoạn 1</h3>
+              </div>
+              <div className="cardInfo-detail">
                 <p>Đăng ký form</p>
-                <Progress type="circle" percent={100} width={0} className="status-icon" />
-                <div onClick={handleClick} className={click ? 'expanded' : 'collapsed'}>
-                  {click ? 'less' : 'more'}
-                  {click ? <AiOutlineArrowUp className="toggle-arrow" /> : <AiOutlineArrowDown className="toggle-arrow" />}
+                <div className="time">
+                  <span className="time-heading">Thời gian: </span>
+                  <span className="time-number">20/09 - 28/09</span>
+                </div>
+                <div className="progress-status">
+                  <AiOutlineCheckCircle className="progress-status-icon" />
+                  <span className="progress-status-text">Đang mở</span>
                 </div>
               </div>
-            </div>
 
-            <div className="challenge__container-card">
-              <div className="challenge__container-cardInfo">
-                <div className="icon">
-                  <GiBookCover />
-                </div>
-                <h3>Giai đoạn 2</h3>
-                <p>Thử thách đầu tiên</p>
-              </div>
-            </div>
-
-            <div className="challenge__container-card">
-              <div className="challenge__container-cardInfo">
-                <div className="icon">
-                  <FaHandshake />
-                </div>
-                <h3>Giai đoạn 3</h3>
-                <p>Thử thách thứ hai</p>
-              </div>
+              {click ? <RiArrowDropUpFill className="toggle-arrow" onClick={handleClick} /> : <RiArrowDropDownFill className="toggle-arrow" onClick={handleClick} />}
             </div>
           </div>
+
+          <div className="challenge__container-card">
+            <div className="challenge__container-cardInfo">
+              <div className="challenge-card-header">
+                <FaFire className="challenge-icon" />
+                <h3>Giai đoạn 2</h3>
+              </div>
+              <div className="cardInfo-detail">
+                <p>Thử thách thứ nhất</p>
+                <div className="time">
+                  <span className="time-heading">Thời gian: </span>
+                  <span className="time-number">29/09 - 07/10</span>
+                </div>
+                <div className="progress-status">
+                  <LoadingOutlined style={{ fontSize: 20, color: 'blue' }} spin className="progress-waiting-icon" />
+                  <span className="progress-status-text">Đang chờ</span>
+                </div>
+              </div>
+              <RiArrowDropDownFill className="toggle-arrow invalid-toggle" />
+            </div>
+          </div>
+
+          <div className="challenge__container-card">
+            <div className="challenge__container-cardInfo">
+              <div className="challenge-card-header">
+                <FaFire className="challenge-icon" />
+                <h3>Giai đoạn 3</h3>
+              </div>
+              <div className="cardInfo-detail">
+                <p>Thử thách thứ hai</p>
+                <div className="time">
+                  <span className="time-heading">Thời gian: </span>
+                  <span className="time-number">29/09 - 07/10</span>
+                </div>
+                <div className="progress-status">
+                  <LoadingOutlined style={{ fontSize: 20, color: 'blue' }} spin className="progress-waiting-icon" />
+                  <span className="progress-status-text">Đang chờ</span>
+                </div>
+              </div>
+              <RiArrowDropDownFill className="toggle-arrow invalid-toggle" />
+            </div>
+          </div>
+
         </div>
       </div>
-    </IconContext.Provider>
+    </div>
+
   );
 }
 
