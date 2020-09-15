0012354f3afff61a115802eda9c63c5cc02c5a68
unknown <khoi@gmail.com>
2020-09-15 21:29:09 +0700

    updated lately

--- src/components/pages/Info page/MyCarousel.js
+++ src/components/pages/Info page/MyCarousel.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import InfiniteCarousel from 'react-leaf-carousel';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Picture1 from './activities-images/activity-1.jpg';
 import Picture2 from './activities-images/activity-2.jpg';
 import Picture3 from './activities-images/activity-3.jpg';
@@ -15,7 +17,14 @@ function MyCarousel() {
       <div className="content-wrapper">
         <div className="text-paragraph">
           <div className="text-paragraph-header">CLB F-CODE</div>
-          <p className="text-paragraph-body">F-Code được thành lập vào năm 2014, là CLB học thuật đầu tiên tại trường đại học FPT HCM. Với mục tiêu ban đầu là tạo ra một nơi để các bạn sinh viên ngành kĩ thuật phần mềm, an toàn thông tin có đam mê về lập trình được thỏa sức trao đổi và học tập với nhau. Sau hơn 5 năm hoạt động, CLB đã tạo được nhiều sân chơi cho các bạn sinh viên trường ĐH FPT.</p>
+          <p className="text-paragraph-body">
+            F-Code được thành lập vào năm 2014, là CLB học thuật đầu tiên tại trường đại học FPT HCM. Với mục tiêu ban đầu là tạo ra một nơi để các bạn sinh viên ngành kĩ thuật phần mềm, an toàn thông tin có đam mê về lập trình được thỏa sức trao đổi và học tập với nhau. Sau hơn 5 năm hoạt động, CLB đã tạo được nhiều sân chơi cho các bạn sinh viên trường ĐH FPT.
+            <span className="visit-link">
+              <Link to="//fcodehcm.wordpress.com" target="_blank">Xem thêm</Link>
+              <span className="link-arrow"><FaArrowRight /></span>
+            </span>
+          </p>
+
         </div>
       </div>
       <InfiniteCarousel
