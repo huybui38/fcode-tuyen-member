c3c0a1c61a738605e661df5b56ae21742511228f
Ngoc Huy <huybui479@gmail.com>
2020-09-20 11:37:13 +0700

    add folder production

---
+++ build/precache-manifest.c6d01471d32330452d12691cc7eff49d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c216c7f5dc7a52735c0623e87283c31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e82c9f0486111304a19",
+    "url": "/static/css/2.c34c98ad.chunk.css"
+  },
+  {
+    "revision": "299fe2d263ea664709bf",
+    "url": "/static/css/main.37cab5cd.chunk.css"
+  },
+  {
+    "revision": "8e82c9f0486111304a19",
+    "url": "/static/js/2.543f0ffc.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.543f0ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299fe2d263ea664709bf",
+    "url": "/static/js/main.22e28106.chunk.js"
+  },
+  {
+    "revision": "bc7f9afdfd728a9e6c0d",
+    "url": "/static/js/runtime-main.675beb0a.js"
+  },
+  {
+    "revision": "6d01897b5bd48812c1aef9a4360157ff",
+    "url": "/static/media/activity-1.6d01897b.jpg"
+  },
+  {
+    "revision": "1345a630d4d0f5eceff18a631a0bd238",
+    "url": "/static/media/activity-2.1345a630.jpg"
+  },
+  {
+    "revision": "2a503e13c18a1fbdf767f87bc14e2ad3",
+    "url": "/static/media/activity-3.2a503e13.jpg"
+  },
+  {
+    "revision": "bcd98d578868990c9639bbaf5b658f9a",
+    "url": "/static/media/activity-4.bcd98d57.jpg"
+  },
+  {
+    "revision": "57cdd2dd2e14e58b0ccd6f5bf67fa7bd",
+    "url": "/static/media/activity-5.57cdd2dd.jpg"
+  },
+  {
+    "revision": "c06660bb73cf6736aedfebcac9c1dd7d",
+    "url": "/static/media/activity-6.c06660bb.jpg"
+  },
+  {
+    "revision": "5b8d95b03dadfc2492ad85366a5e36f6",
+    "url": "/static/media/activity-7.5b8d95b0.jpg"
+  },
+  {
+    "revision": "5a147418d33fc2b7c46b04381df53665",
+    "url": "/static/media/background.5a147418.jpg"
+  },
+  {
+    "revision": "458017c8093f53e0a45089e6557223bb",
+    "url": "/static/media/gg-logo.458017c8.png"
+  },
+  {
+    "revision": "4d32f46b4113d5cb3a60fe3bf85b05fd",
+    "url": "/static/media/logo.4d32f46b.png"
+  },
+  {
+    "revision": "dbeeaf12b52b522f70c73f77d042de54",
+    "url": "/static/media/slogan.dbeeaf12.png"
+  }
+]);
